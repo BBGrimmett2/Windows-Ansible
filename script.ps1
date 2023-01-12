5ea72561d3b78db573df8b95e9c5a702dd58751c
Brian Grimmett <bgrimmet@redhat.com>
2023-01-12 10:05:01 -0500

    edits

--- script.ps1
+++ script.ps1
@@ -1,11 +1,13 @@
 # Adopted from https://github.com/ansible/ansible/blob/devel/examples/scripts/ConfigureRemotingForAnsible.ps1
 #Requires -Version 3.0
 
+# Variable Initalization -----------------------------------------------------------------------------------
+
 [CmdletBinding()]
 
 Param (
     [string]$SubjectName = $env:COMPUTERNAME,
-    [int]$CertValidityDays = 1095,
+    # [int]$CertValidityDays = 1095,
     [switch]$SkipNetworkProfileCheck,
     $CreateSelfSignedCert = $true,
     [switch]$ForceNewSSLCert,
@@ -14,96 +16,83 @@ Param (
     [switch]$EnableCredSSP = $true
 )
 
-# Function Write-ProgressLog {
-#     $Message = $args[0]
-#     Write-EventLog -LogName Application -Source $EventSource -EntryType Information -EventId 1 -Message $Message
-# }
+# Self Signed Cert -----------------------------------------------------------------------------------------
 
-Function Write-OutputLog {
-    $Message = $args[0]
-    Write-Output $Message
-    # Write-ProgressLog $Message
+Function New-LegacySelfSignedCert {
+    Param (
+        [string]$SubjectName,
+        [int]$ValidDays = 1095
+    )
+
+    $hostnonFQDN = $env:computerName
+    $hostFQDN = [System.Net.Dns]::GetHostByName(($env:computerName)).Hostname
+    $SignatureAlgorithm = "SHA256"
+
+    $name = New-Object -COM "X509Enrollment.CX500DistinguishedName.1"
+    $name.Encode("CN=$SubjectName", 0)
+
+    $key = New-Object -COM "X509Enrollment.CX509PrivateKey.1"
+    $key.ProviderName = "Microsoft Enhanced RSA and AES Cryptographic Provider"
+    $key.KeySpec = 1
+    $key.Length = 4096
+    $key.SecurityDescriptor = "D:PAI(A;;0xd01f01ff;;;SY)(A;;0xd01f01ff;;;BA)(A;;0x80120089;;;NS)"
+    $key.MachineContext = 1
+    $key.Create()
+
+    $serverauthoid = New-Object -COM "X509Enrollment.CObjectId.1"
+    $serverauthoid.InitializeFromValue("1.3.6.1.5.5.7.3.1")
+    $ekuoids = New-Object -COM "X509Enrollment.CObjectIds.1"
+    $ekuoids.Add($serverauthoid)
+    $ekuext = New-Object -COM "X509Enrollment.CX509ExtensionEnhancedKeyUsage.1"
+    $ekuext.InitializeEncode($ekuoids)
+
+    $cert = New-Object -COM "X509Enrollment.CX509CertificateRequestCertificate.1"
+    $cert.InitializeFromPrivateKey(2, $key, "")
+    $cert.Subject = $name
+    $cert.Issuer = $cert.Subject
+    $cert.NotBefore = (Get-Date).AddDays(-1)
+    $cert.NotAfter = $cert.NotBefore.AddDays($ValidDays)
+
+    $SigOID = New-Object -ComObject X509Enrollment.CObjectId
+    $SigOID.InitializeFromValue(([Security.Cryptography.Oid]$SignatureAlgorithm).Value)
+
+    [string[]] $AlternativeName += $hostnonFQDN
+    $AlternativeName += $hostFQDN
+    $IAlternativeNames = New-Object -ComObject X509Enrollment.CAlternativeNames
+
+    foreach ($AN in $AlternativeName) {
+        $AltName = New-Object -ComObject X509Enrollment.CAlternativeName
+        $AltName.InitializeFromString(0x3, $AN)
+        $IAlternativeNames.Add($AltName)
+    }
+
+    $SubjectAlternativeName = New-Object -ComObject X509Enrollment.CX509ExtensionAlternativeNames
+    $SubjectAlternativeName.InitializeEncode($IAlternativeNames)
+
+    [String[]]$KeyUsage = ("DigitalSignature", "KeyEncipherment")
+    $KeyUsageObj = New-Object -ComObject X509Enrollment.CX509ExtensionKeyUsage
+    $KeyUsageObj.InitializeEncode([int][Security.Cryptography.X509Certificates.X509KeyUsageFlags]($KeyUsage))
+    $KeyUsageObj.Critical = $true
+
+    $cert.X509Extensions.Add($KeyUsageObj)
+    $cert.X509Extensions.Add($ekuext)
+    $cert.SignatureInformation.HashAlgorithm = $SigOID
+    $CERT.X509Extensions.Add($SubjectAlternativeName)
+    $cert.Encode()
+
+    $enrollment = New-Object -COM "X509Enrollment.CX509Enrollment.1"
+    $enrollment.InitializeFromRequest($cert)
+    $certdata = $enrollment.CreateRequest(0)
+    $enrollment.InstallResponse(2, $certdata, 0, "")
+
+    # extract/return the thumbprint from the generated cert
+    $parsed_cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
+    $parsed_cert.Import([System.Text.Encoding]::UTF8.GetBytes($certdata))
+
+    return $parsed_cert.Thumbprint
 }
 
-# Function Write-HostLog {
-#     $Message = $args[0]
-#     Write-Output $Message
-#     Write-ProgressLog $Message
-# }
-
-# Function New-LegacySelfSignedCert {
-#     Param (
-#         [string]$SubjectName,
-#         [int]$ValidDays = 1095
-#     )
-
-#     $hostnonFQDN = $env:computerName
-#     $hostFQDN = [System.Net.Dns]::GetHostByName(($env:computerName)).Hostname
-#     $SignatureAlgorithm = "SHA256"
-
-#     $name = New-Object -COM "X509Enrollment.CX500DistinguishedName.1"
-#     $name.Encode("CN=$SubjectName", 0)
-
-#     $key = New-Object -COM "X509Enrollment.CX509PrivateKey.1"
-#     $key.ProviderName = "Microsoft Enhanced RSA and AES Cryptographic Provider"
-#     $key.KeySpec = 1
-#     $key.Length = 4096
-#     $key.SecurityDescriptor = "D:PAI(A;;0xd01f01ff;;;SY)(A;;0xd01f01ff;;;BA)(A;;0x80120089;;;NS)"
-#     $key.MachineContext = 1
-#     $key.Create()
-
-#     $serverauthoid = New-Object -COM "X509Enrollment.CObjectId.1"
-#     $serverauthoid.InitializeFromValue("1.3.6.1.5.5.7.3.1")
-#     $ekuoids = New-Object -COM "X509Enrollment.CObjectIds.1"
-#     $ekuoids.Add($serverauthoid)
-#     $ekuext = New-Object -COM "X509Enrollment.CX509ExtensionEnhancedKeyUsage.1"
-#     $ekuext.InitializeEncode($ekuoids)
-
-#     $cert = New-Object -COM "X509Enrollment.CX509CertificateRequestCertificate.1"
-#     $cert.InitializeFromPrivateKey(2, $key, "")
-#     $cert.Subject = $name
-#     $cert.Issuer = $cert.Subject
-#     $cert.NotBefore = (Get-Date).AddDays(-1)
-#     $cert.NotAfter = $cert.NotBefore.AddDays($ValidDays)
-
-#     $SigOID = New-Object -ComObject X509Enrollment.CObjectId
-#     $SigOID.InitializeFromValue(([Security.Cryptography.Oid]$SignatureAlgorithm).Value)
-
-#     [string[]] $AlternativeName += $hostnonFQDN
-#     $AlternativeName += $hostFQDN
-#     $IAlternativeNames = New-Object -ComObject X509Enrollment.CAlternativeNames
-
-#     foreach ($AN in $AlternativeName) {
-#         $AltName = New-Object -ComObject X509Enrollment.CAlternativeName
-#         $AltName.InitializeFromString(0x3, $AN)
-#         $IAlternativeNames.Add($AltName)
-#     }
-
-#     $SubjectAlternativeName = New-Object -ComObject X509Enrollment.CX509ExtensionAlternativeNames
-#     $SubjectAlternativeName.InitializeEncode($IAlternativeNames)
-
-#     [String[]]$KeyUsage = ("DigitalSignature", "KeyEncipherment")
-#     $KeyUsageObj = New-Object -ComObject X509Enrollment.CX509ExtensionKeyUsage
-#     $KeyUsageObj.InitializeEncode([int][Security.Cryptography.X509Certificates.X509KeyUsageFlags]($KeyUsage))
-#     $KeyUsageObj.Critical = $true
-
-#     $cert.X509Extensions.Add($KeyUsageObj)
-#     $cert.X509Extensions.Add($ekuext)
-#     $cert.SignatureInformation.HashAlgorithm = $SigOID
-#     $CERT.X509Extensions.Add($SubjectAlternativeName)
-#     $cert.Encode()
-
-#     $enrollment = New-Object -COM "X509Enrollment.CX509Enrollment.1"
-#     $enrollment.InitializeFromRequest($cert)
-#     $certdata = $enrollment.CreateRequest(0)
-#     $enrollment.InstallResponse(2, $certdata, 0, "")
-
-#     # extract/return the thumbprint from the generated cert
-#     $parsed_cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
-#     $parsed_cert.Import([System.Text.Encoding]::UTF8.GetBytes($certdata))
-
-#     return $parsed_cert.Thumbprint
-# }
+# Firewall Access ------------------------------------------------------------------------------------------
 
 Function Enable-GlobalHttpFirewallAccess {
     Write-Output "Forcing global HTTP firewall access"
@@ -157,41 +146,15 @@ Function Enable-GlobalHttpFirewallAccess {
     Write-Output "HTTP firewall rule $($rule.Name) updated"
 }
 
-# Setup error handling.
-Trap {
-    $_
-    Exit 1
-}
-$ErrorActionPreference = "Stop"
-
-# Get the ID and security principal of the current user account
-$myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-$myWindowsPrincipal = new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
-
-# Get the security principal for the Administrator role
-$adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
-
-# Check to see if we are currently running "as Administrator"
-if (-Not $myWindowsPrincipal.IsInRole($adminRole)) {
-    Write-Output "ERROR: You need elevated Administrator privileges in order to run this script."
-    Write-Output "       Start Windows PowerShell by using the Run as Administrator option."
-    Exit 2
-}
-
-$EventSource = $MyInvocation.MyCommand.Name
-If (-Not $EventSource) {
-    $EventSource = "Powershell CLI"
-}
-
-If ([System.Diagnostics.EventLog]::Exists('Application') -eq $False -or [System.Diagnostics.EventLog]::SourceExists($EventSource) -eq $False) {
-    New-EventLog -LogName Application -Source $EventSource
-}
+# PowerShell Version ---------------------------------------------------------------------------------------
 
 # Detect PowerShell version.
 If ($PSVersionTable.PSVersion.Major -lt 3) {
     # Write-ProgressLog "PowerShell version 3 or higher is required."
     Throw "PowerShell version 3 or higher is required."
 }
+
+# WinRM Service --------------------------------------------------------------------------------------------
 
 # Find and start the WinRM service.
 Write-Output "Verifying WinRM service."
@@ -208,6 +171,8 @@ ElseIf ((Get-Service "WinRM").Status -ne "Running") {
     # Write-ProgressLog "Started WinRM service."
 
 }
+
+# PS Session Config ----------------------------------------------------------------------------------------
 
 # WinRM should be running; check that we have a PS session config.
 If (!(Get-PSSessionConfiguration -Verbose:$false) -or (!(Get-ChildItem WSMan:\localhost\Listener))) {
@@ -226,6 +191,8 @@ Else {
     Write-Output "PS Remoting is already enabled."
 }
 
+# Local Account Token Filter Policy ------------------------------------------------------------------------
+
 # Ensure LocalAccountTokenFilterPolicy is set to 1
 # https://github.com/ansible/ansible/issues/42978
 $token_path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
@@ -240,15 +207,14 @@ if ($token_value -ne 1) {
     New-ItemProperty -Path $token_path -Name $token_prop_name -Value 1 -PropertyType DWORD > $null
 }
 
+# SSL Listener ---------------------------------------------------------------------------------------------
+
 # Make sure there is a SSL listener.
 $listeners = Get-ChildItem WSMan:\localhost\Listener
 If (!($listeners | Where-Object { $_.Keys -like "TRANSPORT=HTTPS" })) {
     # We cannot use New-SelfSignedCertificate on 2012R2 and earlier
-    $thumbprint = New-LegacySelfSignedCert -SubjectName $SubjectName 
+    $thumbprint = New-LegacySelfSignedCert -SubjectName $SubjectName # -ValidDays $CertValidityDays
     # Write-HostLog "Self-signed SSL certificate generated; thumbprint: $thumbprint"
-
-        #  -ValidDays $CertValidityDays
-
 
     # Create the hashtables of settings to be used.
     $valueset = @{
@@ -272,7 +238,7 @@ Else {
     If ($ForceNewSSLCert) {
 
         # We cannot use New-SelfSignedCertificate on 2012R2 and earlier
-        $thumbprint = New-LegacySelfSignedCert -SubjectName $SubjectName -ValidDays $CertValidityDays
+        $thumbprint = New-LegacySelfSignedCert -SubjectName $SubjectName # -ValidDays $CertValidityDays
         # Write-HostLog "Self-signed SSL certificate generated; thumbprint: $thumbprint"
 
         $valueset = @{
@@ -292,6 +258,8 @@ Else {
     }
 }
 
+# Authentication Settings ----------------------------------------------------------------------------------
+
 # Check for basic authentication.
 $basicAuthSetting = Get-ChildItem WSMan:\localhost\Service\Auth | Where-Object { $_.Name -eq "Basic" }
 
@@ -305,18 +273,16 @@ If ($DisableBasicAuth) {
         Write-Output "Basic auth is already disabled."
     }
 }
-Else {
-    If (($basicAuthSetting.Value) -eq $false) {
-        Write-Output "Enabling basic auth support."
-        Set-Item -Path "WSMan:\localhost\Service\Auth\Basic" -Value $true
-        # Write-ProgressLog "Enabled basic auth support."
-    }
-    Else {
-        Write-Output "Basic auth is already enabled."
-    }
-}
-
-
+# Else {
+#     If (($basicAuthSetting.Value) -eq $false) {
+#         Write-Output "Enabling basic auth support."
+#         Set-Item -Path "WSMan:\localhost\Service\Auth\Basic" -Value $true
+#         # Write-ProgressLog "Enabled basic auth support."
+#     }
+#     Else {
+#         Write-Output "Basic auth is already enabled."
+#     }
+# }
 
 # If EnableCredSSP if set to true
 If ($EnableCredSSP) {
@@ -328,6 +294,8 @@ If ($EnableCredSSP) {
         # Write-ProgressLog "Enabled CredSSP auth support."
     }
 }
+
+# Firewall Settings ----------------------------------------------------------------------------------------
 
 If ($GlobalHttpFirewallAccess) {
     Enable-GlobalHttpFirewallAccess
@@ -350,6 +318,8 @@ Else {
     Write-Output "Firewall rule already exists to allow WinRM HTTPS."
 }
 
+# Testing Remote Session -----------------------------------------------------------------------------------
+
 # Test a remoting connection to localhost, which should work.
 $httpResult = Invoke-Command -ComputerName "localhost" -ScriptBlock { $using:env:COMPUTERNAME } -ErrorVariable httpError -ErrorAction SilentlyContinue
 $httpsOptions = New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck
@@ -366,4 +336,7 @@ Else {
     # Write-ProgressLog "Unable to establish an HTTP remoting session."
     Throw "Unable to establish an HTTP remoting session."
 }
-Write-OutputLog "PS Remoting has been successfully configured for Ansible."
+
+# Confirmation ---------------------------------------------------------------------------------------------
+
+Write-Output "PS Remoting has been successfully configured for Ansible."
